ebf4189a6c1855860136d1f6fa3b460e3cc2a7a7
Nitesh Badiya <101646890+nitesh882@users.noreply.github.com>
2024-08-14 00:01:33 +0530

    update the code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,29 +1,35 @@
 package com.driver;
 
 
-public class Product {
-    
-    public int product(int x, int y) {
-        return x * y;
-    }
-    
-    public int product(int x, int y, int z) {
-        return x * y * z;
-    }
-    
-    public double product(double x, double y) {
-        return x * y;
-    }
-}
 public class Main {
 
-    public static void main(String[] args) {
-        Product p = new Product();
-        
-        p.product(2, 4);// Output will be 8
-        p.product(2, 4, 6);     // Output will be 48
-        p.product(2.5, 4.6);    // Output will be 11.5
-    }
-  
+    // Task 1: Create a class Product inside Main class
+    public class Product {
 
+        // Task 3: Create a method product(int x, int y)
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        // Task 4: Create an overloaded method product(int x, int y, int z)
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        // Task 5: Create an overloaded method product(double x, double y)
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Task 2: Create an object of Product in Main function called p
+        Main mainInstance = new Main();
+        Product p = mainInstance.new Product();
+
+        // Call the methods from Main using the Product class object p
+    p.product(2, 4);        // Task 3: Call the first method, Output: 8
+     p.product(2, 4, 6);     // Task 4: Call the second method, Output: 48
+        p.product(2.5, 4.6);    // Task 5: Call the third method, Output: 11.5
+    }
 }
