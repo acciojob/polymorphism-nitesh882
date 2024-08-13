fbd240338be0353571a5377ba90bf04877f643b5
Nitesh Badiya <101646890+nitesh882@users.noreply.github.com>
2024-08-13 22:32:32 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
+
+public class Product {
+    
+    public int product(int x, int y) {
+        return x * y;
+    }
+    
+    public int product(int x, int y, int z) {
+        return x * y * z;
+    }
+    
+    public double product(double x, double y) {
+        return x * y;
+    }
+}
 public class Main {
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        
+        System.out.println(p.product(2, 4));        // Output will be 8
+        System.out.println(p.product(2, 4, 6));     // Output will be 48
+        System.out.println(p.product(2.5, 4.6));    // Output will be 11.5
+    }
+  
 
 }
